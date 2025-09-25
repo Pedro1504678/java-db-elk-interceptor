95d690303b2e3da0f6aa031fc504c6c81c492e00
Pedro Alexandre <pedroalexandre.bastos@hotmail.com>
2025-09-24 22:44:45 -0300

    remove temporally log interceptor

--- src/main/java/com/example/teste/app/config/WebConfig.java
+++ src/main/java/com/example/teste/app/config/WebConfig.java
@@ -1,19 +1,19 @@
-package com.example.teste.app.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Autowired
-    private HttpLoggingInterceptor httpLoggingInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(httpLoggingInterceptor);
-    }
-
-}
+//package com.example.teste.app.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    private HttpLoggingInterceptor httpLoggingInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(httpLoggingInterceptor);
+//    }
+//
+//}
